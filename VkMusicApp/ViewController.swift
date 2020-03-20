a5558181a853bb94ee5b08b2cbd46a6386a3dae3
Artem Shuba <a.shuba@outlook.com>
2020-03-20 20:37:34 +0100

    Initial Commit

---
+++ VkMusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VkMusicApp
+//
+//  Created by Artem Shuba on 20/03/2020.
+//  Copyright © 2020 Artem Shuba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
